4422b51a90587c05f9cb37c4ae04023a1212d252
Vidi Joshubzky Saviola <103508462+TMTMPST@users.noreply.github.com>
2024-09-13 04:43:34 +0700

    new

---
+++ WEEK_3/Tugas/main.js
@@ -0,0 +1,25 @@
+function validateForm() {
+    var nama = document.getElementById("nama").value;
+    var alamat = document.getElementById("alamat").value;
+    var tanggalLahir = document.getElementById("tanggalLahir").value;
+    var centang = document.getElementsByName("check").value;
+    if (nama === "") {
+        alert("nama harus diisi.")
+        return false;
+    }
+    if (alamat === "") {
+        alert("Alamat harus diisi.")
+        return false;
+    }
+    if (tanggalLahir === "") {
+        alert("Tanggal Lahir harus diisi.")
+        return false;
+    }
+    if (centang => 1) {
+        alert("silahkan Centang dulu")
+        return false;
+    }
+    
+    alert("Form submitted successfully!");
+
+}
