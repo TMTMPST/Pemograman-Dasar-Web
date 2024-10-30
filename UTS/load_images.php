b04a30915852c0db45045a70e7125e47968d2512
Vidi Joshubzky Saviola <103508462+TMTMPST@users.noreply.github.com>
2024-10-31 01:05:37 +0700

    adding

--- UTS/load_images.php
+++ UTS/load_images.php
@@ -5,7 +5,6 @@ $images = array();
 if (is_dir($uploadDir)) {
     if ($dh = opendir($uploadDir)) {
         while (($file = readdir($dh)) !== false) {
-            // Skip the current and parent directory
             if ($file != '.' && $file != '..') {
                 $images[] = $uploadDir . $file;
             }
